5523252a540fa32209ceea0d25b5412b7e12bbff
eterna1g <123172256+eterna1g@users.noreply.github.com>
2023-01-20 14:52:16 +0200

    Update web-for-classification.py

--- web-for-classification.py
+++ web-for-classification.py
@@ -3,8 +3,7 @@ import streamlit as st
 from PIL import Image
 from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.efficientnet import preprocess_input
-from tensorflow.keras.applications.efficientnet import decode_predictions
+from tensorflow.keras.applications.efficientnet import preprocess_input, decode_predictions
 import numpy as np
 
 def load_model():
